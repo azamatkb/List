9490545279537bbb1e29a058f8906fe8346fbd34
Azamat <azamataudi@mail.ru>
2021-05-09 19:19:53 +0600

    first commit

---
+++ problem17+.py
@@ -0,0 +1,5 @@
+#17 Взять лист 1 и подсчитать  сколько раз там встречается имя "Jack"
+names = ['Jack', 'Jimmy', 'Jackson', 'Jhon', 'Jackson', 'Jhon',  'Jimmy',
+         'Jackson', 'Jhon','Jack', 'Jimmy', 'Jack', 'Jackson', 'Jhon',
+         'Jackson', 'Jhon','Jack', 'Jimmy', 'Jack', 'Jackson', 'Jhon']
+print(names.count('Jack'))
---
+++ problem72+.py
@@ -0,0 +1,9 @@
+#72 Удалить из лист 1 все четные индексы до 10
+names = ['Jack', 'Jimmy', 'Jackson', 'Jhon', 'Jackson', 'Jhon',  'Jimmy',
+         'Jackson', 'Jhon','Jack', 'Jimmy', 'Jack', 'Jackson', 'Jhon',
+         'Jackson', 'Jhon','Jack', 'Jimmy', 'Jack', 'Jackson', 'Jhon']
+names.pop(2)
+names.pop(3)
+names.pop(4)
+names.pop(5)
+print(names)
